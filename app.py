509b4e9bdb3765e2a1398ffccf1a22fc058e0337
ChandelVatsal <68132483+ChandelVatsal@users.noreply.github.com>
2024-04-29 17:50:38 -0700

    stuff

--- app.py
+++ app.py
@@ -2,16 +2,27 @@ from flask import Flask, render_template, request, session, redirect, url_for
 import openai
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-import keys
+
+# GPT keys
+OPENAPI_BASE = "https://openaiglazko.openai.azure.com/"
+OPENAPI_KEY = "93b0855093794a3f8ee2120a037b7b03"
+OPENAPI_TYPE = "azure"
+OPENAPI_VERS = "2023-05-15"
+OPENAPI_DEPLOYMENT = "GPT35"
+
+
+# Database keys
+DATABASE_URI = "sqlite:///user_history.db"
+SESSION_DB_KEY = "ysecret_key"
 
 app = Flask(__name__)
-app.secret_key = keys.SESSION_DB_KEY
+app.secret_key = SESSION_DB_KEY
 
-openai.api_base = keys.OPENAPI_BASE
-openai.api_key = keys.OPENAPI_KEY
-openai.api_type = keys.OPENAPI_TYPE
-openai.api_version = keys.OPENAPI_VERS
-deployment_name = keys.OPENAPI_DEPLOYMENT
+openai.api_base = OPENAPI_BASE
+openai.api_key = OPENAPI_KEY
+openai.api_type = OPENAPI_TYPE
+openai.api_version = OPENAPI_VERS
+deployment_name = OPENAPI_DEPLOYMENT
 
 
 # Routes
